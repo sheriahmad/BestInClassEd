e572524ee41b9c111af53358b69d4e109c1fcbf8
Sheheryar Ahmad <sheriahmad@hotmail.com>
2018-04-06 18:25:06 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function printNumbers() {
+    for(var i = 0; i <= 100; i++) {
+        if(i % 3 == 0 && i % 5 == 0) {
+            console.log("ThreeFive\n");
+        }
+        else if (i % 3 == 0) {
+            console.log("Three\n");
+        }
+        else if (i % 5 == 0) {
+            console.log("Five\n");
+        }
+        else {
+            console.log("%d", i);
+        }
+    }
+}
+
+
+printNumbers();
